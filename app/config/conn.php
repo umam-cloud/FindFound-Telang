d7946bcb8c52f1bfc00b257d1fc43d63145f63fe
umam-cloud <fatihulumam1617@gmail.com>
2026-05-07 17:54:43 +0700

    feat : membuat constanta variable

--- app/config/conn.php
+++ app/config/conn.php
@@ -6,4 +6,4 @@ define('BASEURL','http://localhost/findfound-telang/public' );
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', '_');
+define('DB_NAME', 'find-found-telang');
