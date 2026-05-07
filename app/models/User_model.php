60b72544e428dfa8275a3b06b2e84e7e374e278d
umam-cloud <fatihulumam1617@gmail.com>
2026-05-07 21:47:18 +0700

    fix: perbaikan variable di user model

--- app/models/User_model.php
+++ app/models/User_model.php
@@ -17,17 +17,15 @@ class User_model{
         $this->db->bind('nama', $data['nama']);
         $this->db->bind('email', $data['email']);
         $this->db->bind('whatsapp', $data['whatsapp']);
-        // Menggunakan password_hash untuk keamanan standar industri
         $this->db->bind('password', password_hash($data['password'], PASSWORD_DEFAULT));
 
         $this->db->execute();
         return $this->db->rowCount();
     }
 
-    public function getUserByEmail($email) {
+    public function getUserByEmail($email){
         $this->db->query('SELECT * FROM ' . $this->table . ' WHERE email = :email');
         $this->db->bind('email', $email);
         return $this->db->resultsingel();
     }
-    
 }
