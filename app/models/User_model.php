44dc75ee7cc6a1da79c1635a787ed64f3237b088
umam-cloud <fatihulumam1617@gmail.com>
2026-05-09 17:35:48 +0700

    feat: tambah method updateData di user model

--- app/models/User_model.php
+++ app/models/User_model.php
@@ -1,7 +1,7 @@
 <?php
 
 class User_model{
-    private $table = 'user';
+    private $table = 'users';
     private $db;
 
     public function __construct(){
@@ -10,7 +10,7 @@ class User_model{
     }
 
     public function tambahData($data) {
-        $query = "INSERT INTO " . $this->table . " (nama_lengkap, email, no_telp, password) 
+        $query = "INSERT INTO " . $this->table . " (nama, email, whatsapp, password) 
                   VALUES(:nama, :email, :whatsapp, :password)";
         
         $this->db->query($query);
@@ -23,9 +23,38 @@ class User_model{
         return $this->db->rowCount();
     }
 
+    public function updateData($data) {
+        $query = "UPDATE " . $this->table . " 
+                  SET nama = :nama, 
+                      email = :email, 
+                      whatsapp = :whatsapp,
+                      lokasi_pilihan = :lokasi,
+                      foto_profil = :profile
+                  WHERE id = :id";
+
+        $this->db->query($query);
+        
+        $this->db->bind('nama', $data['nama']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('whatsapp', $data['whatsapp']);
+        $this->db->bind('lokasi', $data['lokasi']);
+        $this->db->bind('profile', $data['foto_profil']);
+        $this->db->bind('id', $data['id']);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
+
     public function getUserByEmail($email){
         $this->db->query('SELECT * FROM ' . $this->table . ' WHERE email = :email');
         $this->db->bind('email', $email);
+        return $this->db->resultsingel();
+    }
+
+    public function getUserById($id){
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id = :id');
+        $this->db->bind('id', $id);
         return $this->db->resultsingel();
     }
 }
